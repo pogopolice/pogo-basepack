a3f79f3cbb75563fabab7e2c8f1af15b891d5d5f
root <root@puppet.jpcs.lan>
2013-12-26 15:31:55 -0500

    removed make perl and logwatch from package list

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,7 +12,8 @@ class basepack::params {
   }
 
   if $::osfamily == 'RedHat' or $::operatingsystem == 'amazon' {
-    $basepack_list	= [ "make", "crontabs", "sysstat", "cronie", "cronie-anacron", "mailx", "logwatch", "dos2unix", "unix2dos", "sudo", "setools-console", "perl" ]
+#    $basepack_list	= [ "make", "crontabs", "sysstat", "cronie", "cronie-anacron", "mailx", "logwatch", "dos2unix", "unix2dos", "sudo", "setools-console", "perl" ]
+    $basepack_list	= [ "crontabs", "sysstat", "cronie", "cronie-anacron", "mailx", "dos2unix", "unix2dos", "sudo", "setools-console" ]
   } else {
     fail("Class['basepack::params']: Unsupported osfamily: ${::osfamily}")
   }
